79c59457f9251187c0a5fe12e9b960dce53e19f2
Peter <peter@qa.com>
2024-09-17 12:00:18 +0100

    added pepper

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -7,11 +7,15 @@ package com.qa;
 public class App {
     public static void main(String[] args) {
         System.out.println(sayHello());
-        System.out.println(sayHelloToSomeone("Peter2"));
+        System.out.println(sayHelloToSomeone("Peter"));
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
-        System.out.println(sayHelloToSomeone("Peter"));
+        System.out.println(sayHelloToSomeone("Peter M"));
         System.out.println(sayGoodbye());
+        System.out.println(sayHelloToSomeone("Pepper"));;
+    
+    
+    
     }
 
     public static String sayHello() {
